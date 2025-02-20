5cf0feec04d768ae4ef11dc9d3c9dc192f51b49b
Maria Eduarda <eduardabritto2014@gmail.com>
2025-02-20 14:55:15 -0300

    Update Aula 20-02.sql

--- Aula 20-02.sql
+++ Aula 20-02.sql
@@ -1,5 +1,5 @@
 create table cliente (
-id_cliente integer,
+id_cliente serial,
 nome varchar(60),
 cpf varchar(11),
 data_nasc date,
@@ -10,7 +10,7 @@ primary key(id_cliente)
 
 
 create table DVD (
-id_dvd integer, 
+id_dvd serial, 
 titulo varchar(60), 
 genero varchar(60), 
 duracao varchar(60), 
@@ -20,7 +20,7 @@ primary key(id_dvd)
 
 
 create table locacoes (
-id integer,
+id serial,
 id_cliente integer,
 id_dvd integer,
 data date,
@@ -30,19 +30,20 @@ foreign key(id_dvd) references DVD(id_dvd)
 );
 
 
-insert into DVD (id_dvd, titulo, genero, duracao, situacao)
-values (1, 'X-Men', 'Ação', '02:30:00', 'alugado');
+insert into DVD (titulo, genero, duracao, situacao)
+values ('X-Men', 'Ação', '02:30:00', 'alugado');
 
-insert into cliente (id_cliente, nome, cpf, data_nasc, sexo, salario)
-values(1, 'João', '8245738', '1979-10-02', 'M', '1500.00');
+insert into cliente (nome, cpf, data_nasc, sexo, salario)
+values('João', '8245738', '1979-10-02', 'M', '1500.00');
 
-insert into locacoes (id, id_cliente, id_dvd, data)
-values(1, 1, 1, '2025-02-20');
+insert into cliente (nome, cpf, data_nasc, sexo, salario)
+values('Maria', '8245738', '1989-10-02', 'F', '1500.00');
 
+insert into locacoes (id_cliente, id_dvd, data)
+values(1, 1, '2025-02-20');
 
 
 select * from DVD;
 select * from cliente;
 select * from locacoes;
-
 
